084b3f45f5aab2b5419f29f1b1a3777ff5ae9aed
Pavan Gurram <pavan.gurram@publicissapient.com>
2020-05-13 14:25:40 +0100

    WIP

--- src/test/java/runners/testRunner1.java
+++ src/test/java/runners/testRunner1.java
@@ -7,8 +7,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
         features = "classpath:features/",
         glue = {"stepDefinitions"},
-        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}) // Enable it for Jenkins
-//        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:test-output/cucumber-reports/report.html"})
+//        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}) // Enable it for local execution
+//        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:test-output/cucumber-reports/report.html"}) // Enable it for execution on image
+          plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}) // Enable it for Jenkins execution
 
 public class testRunner1 extends AbstractTestNGCucumberTests {
 
