b1939b824dbb3d847a82fafc835d70a8b049107f
Pavan Gurram <pavan.gurram@publicissapient.com>
2020-05-13 12:23:38 +0100

    first commit

---
+++ src/test/java/runners/testRunner1.java
@@ -0,0 +1,15 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+
+        features = "classpath:features/",
+        glue = {"stepDefinitions"},
+//        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"})
+        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:test-output/cucumber-reports/report.html"})
+
+public class testRunner1 extends AbstractTestNGCucumberTests {
+
+}
